c8f694d4779aac5837fdc1495f4cefd5575770b3
DenYermol <yermolenko.d@ajax.systems>
2022-06-29 10:28:24 +0300

    adjust UserCRUDService to use UserCRUDServiceGrpcKt instead of UserCRUDServiceGrpc

--- src/main/kotlin/com/example/grpcmongo/service/UserCRUDService.kt
+++ src/main/kotlin/com/example/grpcmongo/service/UserCRUDService.kt
@@ -3,65 +3,53 @@ package com.example.grpcmongo.service
 import com.example.grpc.*
 import com.example.grpcmongo.repository.UserRepository
 import io.grpc.Status
-import io.grpc.stub.StreamObserver
 import net.devh.boot.grpc.server.service.GrpcService
 import org.springframework.beans.factory.annotation.Autowired
 import com.example.grpcmongo.model.User as UserModel
 
 @GrpcService
-class UserCRUDService : UserCRUDServiceGrpc.UserCRUDServiceImplBase() {
+class UserCRUDService : UserCRUDServiceGrpcKt.UserCRUDServiceCoroutineImplBase() {
 
     @Autowired
     lateinit var repository: UserRepository
 
-    override fun createUser(request: User?, responseObserver: StreamObserver<ResponseMessage>?) {
-        repository.save(UserModel.fromProtoMessage(request!!)).subscribe()
+    override suspend fun createUser(request: User): ResponseMessage {
+        repository.save(UserModel.fromProtoMessage(request)).subscribe()
 
-        val responseMessage = ResponseMessage.newBuilder().setMessage("user ${request.username} was created").build()
-        responseObserver!!.onNext(responseMessage)
-        responseObserver.onCompleted()
+        return ResponseMessage.newBuilder()
+            .setMessage("user ${request.username} was created")
+            .build()
     }
 
-    override fun readUser(request: Username?, responseObserver: StreamObserver<User>?) {
-        val userModel = repository.findByUsername(request!!.username).block()
-        if (userModel == null) {
-            responseObserver!!.onError(
-                Status.NOT_FOUND.withDescription("user '${request.username}' is not found")
-                    .asRuntimeException()
-            )
-            return
-        }
-        val user = User.newBuilder()
+    override suspend fun readUser(request: Username): User {
+        val userModel = repository.findByUsername(request.username).block()
+            ?: throw Status.NOT_FOUND
+                .withDescription("user '${request.username}' is not found")
+                .asRuntimeException()
+
+        return User.newBuilder()
             .setUsername(userModel.username)
             .setFirstName(userModel.firstName)
             .setLastName(userModel.lastName)
             .setAge(userModel.age)
             .build()
-        responseObserver!!.onNext(user)
-        responseObserver.onCompleted()
     }
 
-    override fun updateUser(request: User?, responseObserver: StreamObserver<ResponseMessage>?) {
-        val user = repository.findByUsername(request!!.username).block()
-        if (user == null) {
-            responseObserver!!.onError(
-                Status.NOT_FOUND.withDescription("user '${request.username}' is not found")
-                    .asRuntimeException()
-            )
-            return
-        }
+    override suspend fun updateUser(request: User): ResponseMessage {
+        val user = repository.findByUsername(request.username).block()
+            ?: throw Status.NOT_FOUND
+                .withDescription("user '${request.username}' is not found")
+                .asRuntimeException()
         repository.save(
             UserModel.fromProtoMessage(request, user.id)
         ).subscribe()
-        val responseMessage = ResponseMessage.newBuilder().setMessage("update user was called").build()
-        responseObserver!!.onNext(responseMessage)
-        responseObserver.onCompleted()
+        return ResponseMessage.newBuilder().setMessage("update user was called").build()
     }
 
-    override fun deleteUser(request: Username?, responseObserver: StreamObserver<ResponseMessage>?) {
-        repository.deleteByUsername(request!!.username).subscribe()
-        val responseMessage = ResponseMessage.newBuilder().setMessage("user '${request.username}' was deleted").build()
-        responseObserver!!.onNext(responseMessage)
-        responseObserver.onCompleted()
+    override suspend fun deleteUser(request: Username): ResponseMessage {
+        repository.deleteByUsername(request.username).subscribe()
+        return ResponseMessage.newBuilder()
+            .setMessage("user '${request.username}' was deleted")
+            .build()
     }
 }
